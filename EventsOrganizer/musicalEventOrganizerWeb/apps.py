755f725ed912ac3a742b4a720621cf3db9d585a5
Gyks <gyksqop@gmail.com>
2019-04-26 15:50:30 +0300

    добавлен скелет приложения, интерфейсно реализованные классы, модели для ORM

---
+++ EventsOrganizer/musicalEventOrganizerWeb/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MusicaleventorganizerwebConfig(AppConfig):
+    name = 'musicalEventOrganizerWeb'
---
+++ EventsOrganizer/musicalEventOrganizerWeb/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    pass
+
+
+def fetch_user(request):
+    pass
+
+
+def fetch_cards(request):
+    pass
+
+
+def fetch_event(request):
+    pass
+
+
+def fetch_contract(request):
+    pass
+
+
+def set_contract(request):
+    pass
+
+
+def add_user(request):
+    pass
+
+
+def add_card(request):
+    pass
